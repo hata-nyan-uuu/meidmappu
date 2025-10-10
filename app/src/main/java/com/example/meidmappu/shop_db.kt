3de2b72a3461712f75df17389bb283264e82dc7f
x24n013 <x24n013@mistuhashi.ac.jp>
2025-10-10 11:04:27 +0900

    1100

--- app/src/main/java/com/example/meidmappu/shop_db.kt
+++ app/src/main/java/com/example/meidmappu/shop_db.kt
@@ -1,21 +1,20 @@
 package com.example.meidmappu
 
 import android.os.Bundle
-import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.Volley
-import com.bumptech.glide.Glide
 
 class shop_dbActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_db)
 
         val container = findViewById<LinearLayout>(R.id.shopContainer)
-        val url = "http://192.168.0.10/meid_app/get_shops.php" // ←あなたのPCのIPアドレスに置き換えて！
+        val url = "http://10.0.2.2/api/get_data.php" // ローカルサーバー
 
         val queue = Volley.newRequestQueue(this)
         val request = JsonArrayRequest(url,
@@ -25,18 +24,25 @@ class shop_dbActivity : AppCompatActivity() {
                     val view = layoutInflater.inflate(R.layout.item_shop, container, false)
 
                     val nameView = view.findViewById<TextView>(R.id.shopName)
+                    val addressView = view.findViewById<TextView>(R.id.shopAddress)
+                    val conceptView = view.findViewById<TextView>(R.id.shopConcept)
                     val menuView = view.findViewById<TextView>(R.id.shopMenu)
+                    val feelingView = view.findViewById<TextView>(R.id.shopFeeling)
+                    val typeView = view.findViewById<TextView>(R.id.shopType)
                     val priceView = view.findViewById<TextView>(R.id.shopPrice)
-                    val imageView = view.findViewById<ImageView>(R.id.shopImage)
+                    val timesView = view.findViewById<TextView>(R.id.shopTimes)
 
-                    nameView.text = shop.getString("name")
-                    menuView.text = shop.getString("menu")
-                    priceView.text = "¥${shop.getInt("price")}"
+                    // JSON データをセット
+                    nameView.text = "店名: ${shop.getString("name")}"
+                    addressView.text = "住所: ${shop.getString("address")}"
+                    conceptView.text = "コンセプト: ${shop.getString("concept")}"
+                    menuView.text = "メニュー: ${shop.getString("menu")}"
+                    feelingView.text = "雰囲気: ${shop.getString("feeling")}"
+                    typeView.text = "種類: ${shop.getString("type")}"
+                    priceView.text = "価格帯: ${shop.getString("price_range")}"
+                    timesView.text = "営業時間: ${shop.getString("times")}"
 
-                    Glide.with(this)
-                        .load(shop.getString("image_url"))
-                        .into(imageView)
-
+                    // LinearLayout に追加
                     container.addView(view)
                 }
             },
